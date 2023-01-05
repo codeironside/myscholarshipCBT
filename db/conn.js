f0af2e4e1901978b61ef97aabf3e7576503d3f8a
cleverAkanimoh <crushclever1@gmail.com>
2023-01-05 19:49:56 +0100

    modified 5 files

--- db/conn.js
+++ db/conn.js
@@ -1,8 +1,27 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
 const connectionString = process.env.MONGODB_URI;
-
-module.exports = mongoose.connect(connectionString, {
+const client = new MongoClient(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => console.log('you have successfully connected to mongodb database')).catch(()=> console.log('failed to connect to database'))
+});
+
+let dbConnection;
+
+module.exports = {
+  connectToServer: function (callback) {
+    client.connect(function (err, db) {
+      if (err || !db) {
+        return callback(err);
+      }
+
+      dbConnection = db.db("sample_airbnb");
+      console.log("Successfully connected to MongoDB.");
+
+      return callback();
+    });
+  },
+
+  getDb: function () {
+    return dbConnection;
+  },
+};
--- server.js
+++ server.js
@@ -1,24 +1,38 @@
 const express = require("express");
 const { errorHandler } = require("./middleware/errormiddleware.js");
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 
 
-app=express()
+const app = express()
+
+// db
+
+// const mongoose = require("mongoose");
+// const connectionString = process.env.MONGODB_URI;
+
+// module.exports = mongoose.connect(connectionString, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }).then(() => console.log('you have successfully connected to mongodb database')).catch(()=> console.log('failed to connect to database'))
 
 //json
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 //router routes
-app.use("/user",require("./routes/user"))
+app.get('./', (req, res) => {
+    res.status(200).send('hompage')
+})
 
-
+app.use("/user", require("./routes/user"))
 
 //error handler
 app.use(errorHandler);
+
 //port number
-const PORT= process.env.PORT||2000
+const PORT = process.env.PORT || 2000
 
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`server listening on  port ${PORT}`)
 })
